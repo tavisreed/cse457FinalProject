b68c4ff03800a3c64b23d29fde0de3f8d1bb250d
LAPTOP-IC83BPPD\tavis <tavisreed@wustl.edu>
2019-04-01 22:55:52 -0500

    Implemented Pie chars

---
+++ src/assets/js/pie.js
@@ -0,0 +1,65 @@
+class Pie {
+    constructor(_parent, _data) {
+        this.parent = _parent;
+        this.data = _data;
+        this.init();
+    }
+
+    init(argument) {
+        let self = this;
+
+        // initialize plot
+        self.margin = {top: 30, right: 30, bottom: 30, left: 60};
+        self.width = window.innerWidth/2.2 - self.margin.left - self.margin.right;
+        self.height = 200 - self.margin.top - self.margin.bottom;
+        self.radius= Math.min(self.width, self.height)/2
+        // set data
+        let pie_data = [self.data.graduate_enroll,self.data.undergrad_enroll];
+
+        var svg = d3.select('#' + self.parent).html('')
+                .attr("width", self.width + self.margin.left + self.margin.right)
+                .attr("height", self.height + self.margin.top + self.margin.bottom)
+                .append("g").attr("transform", "translate(" + self.width/2 + "," + self.height/2 + ")");
+
+
+        //source:https://bl.ocks.org/santi698/f3685ca8a1a7f5be1967f39f367437c0
+
+        var color = d3.scaleOrdinal()
+            .range(["#ffc6e8", "#99ff99"]);
+
+        var arc = d3.arc()
+            .outerRadius(self.radius - 10)
+            .innerRadius(0);
+
+        var labelArc = d3.arc()
+            .outerRadius(self.radius - 40)
+            .innerRadius(self.radius - 40);
+
+        var pie = d3.pie()
+            .sort(null)
+            .value(function(d) { return d; });
+
+        var g = svg.selectAll(".arc")
+            .data(pie(pie_data))
+            .enter().append("g")
+            .attr("class", "arc");
+
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function(d) { return color(d.data); });
+        g.append("text")
+            .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+            .attr("dy", ".35em")
+            .text(function(d,i) {
+                if (d.data==0){
+                    return "";
+                }
+                else if (i==0){
+                    return "G: "+d.data;
+                }
+                else{
+                    return "U: "+d.data;
+                }
+                 });
+    }
+}
--- src/assets/js/school_profile.js
+++ src/assets/js/school_profile.js
@@ -10,19 +10,33 @@ class Profile {
 
     // initialize dropdown menu
     let select = document.querySelector('#selection');
+    let year_select=document.querySelector('#year_selection')
+    let years= [1999,2000,2001,2002,2003,
+      2004,2005,2006,2007,2008,2009,2010,
+      2011,2012,2013,2014,2015,2016,2017,2018];
     let schools = self.data['2018'].map(function(d) { return d.name; })
     for (let k=0; k<schools.length; k++) {
       select.options[select.options.length] = new Option(schools[k], k);
+    }
+    for (let p=0; p<years.length; p++) {
+      year_select.options[year_select.options.length] = new Option(years[p], p);
     }
 
     // initialize dropdown event listener
     select.addEventListener('change', function() {
       let school_idx = select.value;
-      self.load_profile(school_idx);
+      let year_idx=year_select.value;
+      self.load_profile(school_idx, year_idx);
+    });
+
+    year_select.addEventListener('change', function() {
+      let school_idx = select.value;
+      let year_idx=year_select.value;
+      self.load_profile(school_idx, year_idx);
     });
   }
 
-  load_profile(school_idx) {
+  load_profile(school_idx, year_idx) {
     let self = this;
     let name = self.data['2018'][school_idx].name;
     let description = self.data['2018'][school_idx].description;
@@ -48,10 +62,25 @@ class Profile {
       }
     });
 
+    var pie_data=[]
+    var year= parseInt(year_idx)+1999
+    var current_year=self.data[year];
+    for (var i=0; i<current_year.length;i++){
+      if (school_idx==i){
+        pie_data=current_year[i];
+      }
+    }
+
+
+
     // create tuition line charts
     let tuition_line = new Line('tuition_line', tuition_data);
     let enroll_line = new Line('enroll_line', enroll_data);
 
+    // create Pie chart
+    if(pie_data.graduate_enroll!=0 || pie_data.undergrad_enroll!=0 ){
+      let pie_chart=new Pie('GvU_chart', pie_data);
+    }
     // make profile content visible
     document.querySelector('#profiles #content').style.display = 'block';
   }
