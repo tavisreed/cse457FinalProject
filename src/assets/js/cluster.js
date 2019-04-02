360b04b173a6a771f9f9c5853e1a937a04a5443c
Sam Griesemer <samgriesemer@gmail.com>
2019-04-02 16:07:36 -0500

    small updates

--- src/assets/js/cluster.js
+++ src/assets/js/cluster.js
@@ -12,115 +12,112 @@ class Cluster {
     self.margin = {top: 30, right: 30, bottom: 30, left: 60};
     self.width = window.innerWidth/2.2 - self.margin.left - self.margin.right;
     self.height = 200 - self.margin.top - self.margin.bottom;
+    self.padding = 1.5, // separation between same-color nodes
+    self.clusterPadding = 6, // separation between different-color nodes
+    self.maxRadius = 12;
 
     // set data
     let line_data = self.data;
 
-var width = 960,
-    height = 500,
-    padding = 1.5, // separation between same-color nodes
-    clusterPadding = 6, // separation between different-color nodes
-    maxRadius = 12;
+    let n = 200, // total number of nodes
+        m = 10; // number of distinct clusters
 
-var n = 200, // total number of nodes
-    m = 10; // number of distinct clusters
+    let color = d3.scale.category10()
+        .domain(d3.range(m));
 
-var color = d3.scale.category10()
-    .domain(d3.range(m));
+    // The largest node for each cluster.
+    let clusters = new Array(m);
 
-// The largest node for each cluster.
-var clusters = new Array(m);
-
-var nodes = d3.range(n).map(function() {
-  var i = Math.floor(Math.random() * m),
-      r = Math.sqrt((i + 1) / m * -Math.log(Math.random())) * maxRadius,
-      d = {
-        cluster: i,
-        radius: r,
-        x: Math.cos(i / m * 2 * Math.PI) * 200 + width / 2 + Math.random(),
-        y: Math.sin(i / m * 2 * Math.PI) * 200 + height / 2 + Math.random()
-      };
-  if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = d;
-  return d;
-});
-
-var force = d3.layout.force()
-    .nodes(nodes)
-    .size([width, height])
-    .gravity(.02)
-    .charge(0)
-    .on("tick", tick)
-    .start();
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-var node = svg.selectAll("circle")
-    .data(nodes)
-  .enter().append("circle")
-    .style("fill", function(d) { return color(d.cluster); })
-    .call(force.drag);
-
-node.transition()
-    .duration(750)
-    .delay(function(d, i) { return i * 5; })
-    .attrTween("r", function(d) {
-      var i = d3.interpolate(0, d.radius);
-      return function(t) { return d.radius = i(t); };
+    let nodes = d3.range(n).map(function() {
+      let i = Math.floor(Math.random() * m),
+          r = Math.sqrt((i + 1) / m * -Math.log(Math.random())) * maxRadius,
+          d = {
+            cluster: i,
+            radius: r,
+            x: Math.cos(i / m * 2 * Math.PI) * 200 + self.width / 2 + Math.random(),
+            y: Math.sin(i / m * 2 * Math.PI) * 200 + self.height / 2 + Math.random()
+          };
+      if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = d;
+      return d;
     });
 
-function tick(e) {
-  node
-      .each(cluster(10 * e.alpha * e.alpha))
-      .each(collide(.5))
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
-}
+    let force = d3.layout.force()
+        .nodes(nodes)
+        .size([self.width, self.height])
+        .gravity(.02)
+        .charge(0)
+        .on("tick", tick)
+        .start();
 
-// Move d to be adjacent to the cluster node.
-function cluster(alpha) {
-  return function(d) {
-    var cluster = clusters[d.cluster];
-    if (cluster === d) return;
-    var x = d.x - cluster.x,
-        y = d.y - cluster.y,
-        l = Math.sqrt(x * x + y * y),
-        r = d.radius + cluster.radius;
-    if (l != r) {
-      l = (l - r) / l * alpha;
-      d.x -= x *= l;
-      d.y -= y *= l;
-      cluster.x += x;
-      cluster.y += y;
+    let svg = d3.select("body").append("svg")
+        .attr("width", self.width)
+        .attr("height", self.height);
+
+    let node = svg.selectAll("circle")
+        .data(nodes)
+      .enter().append("circle")
+        .style("fill", function(d) { return color(d.cluster); })
+        .call(force.drag);
+
+    node.transition()
+        .duration(750)
+        .delay(function(d, i) { return i * 5; })
+        .attrTween("r", function(d) {
+          let i = d3.interpolate(0, d.radius);
+          return function(t) { return d.radius = i(t); };
+        });
+
+    function tick(e) {
+      node
+          .each(cluster(10 * e.alpha * e.alpha))
+          .each(collide(.5))
+          .attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
     }
-  };
-}
 
-// Resolves collisions between d and all other circles.
-function collide(alpha) {
-  var quadtree = d3.geom.quadtree(nodes);
-  return function(d) {
-    var r = d.radius + maxRadius + Math.max(padding, clusterPadding),
-        nx1 = d.x - r,
-        nx2 = d.x + r,
-        ny1 = d.y - r,
-        ny2 = d.y + r;
-    quadtree.visit(function(quad, x1, y1, x2, y2) {
-      if (quad.point && (quad.point !== d)) {
-        var x = d.x - quad.point.x,
-            y = d.y - quad.point.y,
+    // Move d to be adjacent to the cluster node.
+    function cluster(alpha) {
+      return function(d) {
+        let cluster = clusters[d.cluster];
+        if (cluster === d) return;
+        let x = d.x - cluster.x,
+            y = d.y - cluster.y,
             l = Math.sqrt(x * x + y * y),
-            r = d.radius + quad.point.radius + (d.cluster === quad.point.cluster ? padding : clusterPadding);
-        if (l < r) {
+            r = d.radius + cluster.radius;
+        if (l != r) {
           l = (l - r) / l * alpha;
           d.x -= x *= l;
           d.y -= y *= l;
-          quad.point.x += x;
-          quad.point.y += y;
+          cluster.x += x;
+          cluster.y += y;
         }
-      }
-      return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-    });
-  };
+      };
+    }
+
+    // Resolves collisions between d and all other circles.
+    function collide(alpha) {
+      let quadtree = d3.geom.quadtree(nodes);
+      return function(d) {
+        let r = d.radius + maxRadius + Math.max(self.padding, self.clusterPadding),
+            nx1 = d.x - r,
+            nx2 = d.x + r,
+            ny1 = d.y - r,
+            ny2 = d.y + r;
+        quadtree.visit(function(quad, x1, y1, x2, y2) {
+          if (quad.point && (quad.point !== d)) {
+            let x = d.x - quad.point.x,
+                y = d.y - quad.point.y,
+                l = Math.sqrt(x * x + y * y),
+                r = d.radius + quad.point.radius + (d.cluster === quad.point.cluster ? self.padding : self.clusterPadding);
+            if (l < r) {
+              l = (l - r) / l * alpha;
+              d.x -= x *= l;
+              d.y -= y *= l;
+              quad.point.x += x;
+              quad.point.y += y;
+            }
+          }
+          return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+        });
+      };
 }
