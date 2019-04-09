e850c1f0c9e4b3a0a69420cd236193043a651e33
LAPTOP-IC83BPPD\tavis <tavisreed@wustl.edu>
2019-04-09 17:40:59 -0500

    Still working on stacked area

--- src/assets/js/stacked_area.js
+++ src/assets/js/stacked_area.js
@@ -20,6 +20,15 @@ class Sarea {
         let data =self.data;
         console.log(data);
 
+        var max_enroll=0;
+
+        for (var i=0;i<data.length;i++){
+            var combined=data[i].graduate_enroll+data[i].undergrad_enroll;
+            if (combined>max_enroll){
+                max_enroll=combined;
+            }
+        }
+
         var svg = d3.select('#' + self.parent).html('')
             .attr("width", self.width + self.margin.left + self.margin.right)
             .attr("height", self.height + self.margin.top + self.margin.bottom);
@@ -27,10 +36,12 @@ class Sarea {
         var g=svg.append("g").attr("transform", "translate(" + self.width/2 + "," + self.height/2 + ")");
 
         var x_scale=d3.scaleTime()
-            .range([0, self.width*.95]);
+            .range([0, self.width*.95])
+            .domain([data[0].date.getYear,data[data.length-1].date.getYear]);
 
         var y_scale=d3.scaleLinear()
-            .range([self.height,0]);
+            .range([self.height,0])
+            .domain([0,max_enroll])
 
         var color_scale=d3.scaleOrdinal()
             .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
@@ -41,5 +52,11 @@ class Sarea {
 
         var yAxis = d3.axisLeft()
             .scale(y_scale);
+
+        var area = d3.area()
+            .x(function(d) {
+                return x(d.data.date); })
+            .y0(function(d) { return y(d[0]); })
+            .y1(function(d) { return y(d[1]); });
     }
 }
